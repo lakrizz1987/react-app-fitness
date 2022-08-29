bcc4b357cfde1e32672b65262b1791e0145680e1
Ivaylo <ignatov.ivaylo@gmail.com>
2022-08-29 12:24:50 +0300

    create home page

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 
 import './App.css';
 import Heder from "../src/Components/Header/Header"
-
+import Home from './Components/Home/Home';
 function App() {
   return (
     <div className="App">
       <Heder/>
+      <Home/>
     </div>
   );
 }
---
+++ src/Components/Home/Home.js
@@ -0,0 +1,19 @@
+
+import "../Home/Home.css"
+
+function Home() {
+
+    return (
+        <section className="home-wraper">
+            <article className="home-content">
+                <div className="home-content-box">
+                    <h1 className="home-title">You Are The Beast!</h1>
+                    <p className="home-text">This is an application where you can look at workouts and choose the right ones for you. Take care of yourself and your body!</p>
+                </div>
+
+            </article>
+        </section>
+    )
+}
+
+export default Home;
