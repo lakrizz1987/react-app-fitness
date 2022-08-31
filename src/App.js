d58912ceb986d6e766da54fc147a09e14809d08c
Ivaylo <ignatov.ivaylo@gmail.com>
2022-08-31 11:56:51 +0300

    create footer

--- src/App.js
+++ src/App.js
@@ -6,25 +6,22 @@ import Login from './Components/Autentication/Login';
 import Register from './Components/Autentication/Register';
 import Catalog from './Components/Catalog/Catalog';
 import Details from './Components/Details/Details';
-import  Logout  from './Components/Details/Logout';
+import Logout from './Components/Details/Logout';
+import Footer from './Components/Footer/Footer';
 
 function App() {
   return (
     <div className="App">
       <Heder />
       <Routes>
-        <Route path='/login' element={<Login/>}/>
-        <Route path='/register' element={<Register/>}/>
-        <Route path='/' element={<Home/>}/>
-        <Route path='/catalog' element={<Catalog/>}/>
-        <Route path='/details' element={<Details/>}/>
-        <Route path='/logout' element={<Logout/>}/>
+        <Route path='/login' element={<Login />} />
+        <Route path='/register' element={<Register />} />
+        <Route path='/' element={<Home />} />
+        <Route path='/catalog' element={<Catalog />} />
+        <Route path='/details' element={<Details />} />
+        <Route path='/logout' element={<Logout />} />
       </Routes>
-      <footer>
-        <div>
-          <p style={{'color':'white'}}>Design Ivaylo Ignatov</p>
-        </div>
-      </footer>
+      <Footer />
     </div>
   );
 }
---
+++ src/Components/Footer/Footer.js
@@ -0,0 +1,13 @@
+import "./Footer.css"
+
+function Footer(){
+    return(
+        <footer>
+        <div>
+          <p style={{'color':'white'}}>Design Ivaylo Ignatov</p>
+        </div>
+      </footer>
+    )
+};
+
+export default Footer;
