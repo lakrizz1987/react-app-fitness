ece40a9c0578df3ea16e51a60bc4cf62a955a0b2
Ivaylo <ignatov.ivaylo@gmail.com>
2022-09-01 14:57:22 +0300

    create getOne service

--- src/Components/Details/Details.js
+++ src/Components/Details/Details.js
@@ -2,15 +2,15 @@ import "./Details.css"
 import ReactPlayer from 'react-player';
 import { useState, useEffect } from 'react';
 import { useParams } from 'react-router-dom'
+import { getOne } from "../services/api";
 
 function Details() {
     const [training, setTraining] = useState('');
     const match = useParams();
     const id = match.id
-    
+
     useEffect(() => {
-        fetch(`http://localhost:3030/data/gym/${id}`)
-            .then(res => res.json())
+        getOne(id)
             .then(data => setTraining(data))
     }, [id])
 
--- src/Components/services/api.js
+++ src/Components/services/api.js
@@ -1,4 +1,9 @@
 export const getAll = () => {
     return fetch('http://localhost:3030/data/gym')
         .then(res => res.json())
-} 
+};
+
+export const getOne = (id) => {
+    return fetch(`http://localhost:3030/data/gym/${id}`)
+        .then(res => res.json())
+}
