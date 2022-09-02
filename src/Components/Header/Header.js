fccab920b1bf580135277b7bac77b48dc9eb8954
Ivaylo <ignatov.ivaylo@gmail.com>
2022-09-02 10:59:05 +0300

    fix mobile menu auto hide when click link

--- src/Components/Header/Header.js
+++ src/Components/Header/Header.js
@@ -1,13 +1,14 @@
 import "./Header.css"
 import { Link, useNavigate } from 'react-router-dom'
 import AuthContext from "../../context/AuthContext"
-import { useContext } from "react";
+import { useContext, useState } from "react";
 import { logoutService } from "../services/api";
 
 function Header() {
     const { login, user } = useContext(AuthContext);
     const navigate = useNavigate();
-    
+    const [state, setState] = useState(false)
+
     function logoutHandler(e) {
         e.preventDefault();
         logoutService(user);
@@ -18,21 +19,21 @@ function Header() {
 
     return (
         <div className="nav">
-            <input type="checkbox" id="nav-check" />
+            <input type="checkbox" id="nav-check" checked={state} readOnly />
             <div className="nav-header">
                 <div className="nav-title">
                     FitnessApp.
                 </div>
             </div>
-            <div className="nav-btn">
-                <label htmlFor="nav-check">
+            <div className="nav-btn" onClick={() => setState(true)}>
+                <label htmlFor="nav-check" >
                     <span></span>
                     <span></span>
                     <span></span>
                 </label>
             </div>
 
-            <div className="nav-links">
+            <div className="nav-links" onClick={(e) => { setState(false) }}>
                 <Link className="links" to={'/'}>Home</Link>
                 <Link className="links" to={'/catalog'}>Catalog</Link>
                 <Link className="links" to={'/login'}>Login</Link>
