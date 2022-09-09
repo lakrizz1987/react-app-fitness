61d4bb3e99a0bc6f546ca07c2382f04663c95b85
Ivaylo <ignatov.ivaylo@gmail.com>
2022-09-09 11:41:00 +0300

    create favorites page

--- src/Components/Favorites/Favorites.js
+++ src/Components/Favorites/Favorites.js
@@ -1,13 +1,14 @@
 import { useContext, useEffect, useState } from "react";
 import AuthContext from '../../context/AuthContext'
+import { getOne } from "../../services/api";
 import CatalogCard from "../Catalog/CatalogCard";
 import "./Favorites.css"
 
 
 function Favorites() {
     const { user } = useContext(AuthContext)
-    const [tranings, setTranings] = useState([]);
-    const [display, setDisplay] = useState([]);
+    const [traningsId, setTraningsId] = useState([]);
+    const [exercises, setExercises] = useState([]);
 
     useEffect(() => {
         fetch('http://localhost:3030/data/likes/')
@@ -16,22 +17,33 @@ function Favorites() {
                 const filter = data.filter(x => x._ownerId === user._id);
                 const arr = [];
                 filter.forEach(x => arr.push(x.favorites))
-                setTranings(arr)
+                setTraningsId(arr)
             })
             .catch(err => {
                 alert(err)
             });
 
-    }, [])
+    }, [user._id])
 
-    
-    console.log(tranings)
+    useEffect(() => {
+        traningsId.forEach(x => {
+            getOne(x)
+                .then(data => {
+                    setExercises(oldState => {
+                        return [...oldState, data]
+                    })
+                })
+        })
+    }, [traningsId])
 
-    //return (
-    //<section className="formContainer-fav">
 
-    //</section>
-    //)
+
+
+    return (
+        <section className="formContainer-fav">
+            {exercises.map(x => <CatalogCard ex={x} key={x._id} />)}
+        </section>
+    )
 }
 
 export default Favorites
