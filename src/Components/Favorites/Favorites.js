af67ef9673ca2ed205cd0b5152f5cc6a40f86852
Ivaylo Ignatov <ignatov.ivaylo@gmail.com>
2022-09-15 16:28:18 +0300

    code arrangement

--- src/Components/Favorites/Favorites.js
+++ src/Components/Favorites/Favorites.js
@@ -1,12 +1,15 @@
-import { useContext, useState } from "react";
+import "./Favorites.css";
+
 import MyTrainigContext from "../../context/MyTrainingContext";
 import CatalogCard from "../Catalog/CatalogCard";
 import Loader from "../Loader/Loader";
-import "./Favorites.css"
+
+import { useContext, useState } from "react";
 
 
 
 function Favorites() {
+
     const [timer, setTimer] = useState(false);
 
     const { exercises } = useContext(MyTrainigContext);
@@ -14,25 +17,16 @@ function Favorites() {
     setTimeout(() => {
         setTimer(true);
     }, 1400);
-    /*function removeFavIds(id) {
-        setTraningsId(oldState => {
-            const arr = oldState.filter(x => x !== id);
-            return arr;
-        })
-    }
-    */
-
-
 
     const forRender = (
         (exercises.length === 0) ? <h1 className="no-trainings">No trainings in yor list!</h1> : exercises.map(x => <CatalogCard ex={x} key={x._id} />)
-    )
+    );
 
     return (
         <section className="formContainer-fav">
             {timer ? forRender : <Loader />}
         </section>
-    )
-}
+    );
+};
 
 export default Favorites;
