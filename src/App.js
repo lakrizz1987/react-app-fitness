fe4ede73c96ce5ca308a8a504616d0c3ab70e1f4
Ivaylo <ignatov.ivaylo@gmail.com>
2022-09-16 09:04:39 +0300

    code arrangement

--- src/App.js
+++ src/App.js
@@ -1,35 +1,36 @@
-import { Routes, Route } from 'react-router-dom'
 import './App.css';
-import Heder from "../src/Components/Header/Header"
+
+import Header from "../src/Components/Header/Header";
 import Home from './Components/Home/Home';
 import Login from './Components/Autentication/Login';
 import Register from './Components/Autentication/Register';
 import Catalog from './Components/Catalog/Catalog';
-import Details from './Components/Details/Details';
-import Footer from './Components/Footer/Footer';
 import All from './Components/Catalog/Category/All';
 import Chest from './Components/Catalog/Category/Chest';
 import Back from './Components/Catalog/Category/Back';
 import Arms from './Components/Catalog/Category/Arms';
 import Shoulders from './Components/Catalog/Category/Shoulders';
 import Legs from './Components/Catalog/Category/Legs';
+import Details from './Components/Details/Details';
 import Favorites from './Components/Favorites/Favorites';
-import { AuthProvider } from "./context/AuthContext"
-import { MyTrainigProvider } from './context/MyTrainingContext'
+import Footer from './Components/Footer/Footer';
+
+import { Routes, Route } from 'react-router-dom';
+import { AuthProvider } from "./context/AuthContext";
+import { MyTrainigProvider } from './context/MyTrainingContext';
 
 function App() {
-
 
 
     return (
         <div className="App">
             <AuthProvider>
                 <MyTrainigProvider>
-                    <Heder />
+                    <Header />
                     <Routes>
+                        <Route path='/' element={<Home />} />
                         <Route path='/login' element={<Login />} />
                         <Route path='/register' element={<Register />} />
-                        <Route path='/' element={<Home />} />
                         <Route path='/catalog' element={<Catalog />}>
                             <Route path='all' element={<All />}></Route>
                             <Route path='chest' element={<Chest />}></Route>
@@ -46,6 +47,6 @@ function App() {
             </AuthProvider>
         </div>
     );
-}
+};
 
 export default App;
